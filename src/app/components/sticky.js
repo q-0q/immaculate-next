b2f5e8a9f22022b1bb3afd0e5466e24e76b9ead4
Jack Withers <jackwithers@Jacks-MacBook-Pro.local>
2024-02-10 13:06:30 -0500

    add padding to breakpoint

--- src/app/components/sticky.js
+++ src/app/components/sticky.js
@@ -11,12 +11,12 @@ export default function Sticky() {
 
         function lerpScroll(start, end) {
             var unclamped = (scrollY - start) / (end - start)
-            return Math.min(Math.max(unclamped, 0), 0.999)
+            return Math.min(Math.max(unclamped, 0), 0.99999)
         }
 
         window.addEventListener('scroll', () => {
             var el = document.getElementById("sticky");
-            if (lerpScroll(0, window.screen.availHeight) > 0.998) {
+            if (lerpScroll(0, window.innerHeight) > 0.99998) {
                 el.classList.add("sticky");
             }
             else {
